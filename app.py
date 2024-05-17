76ede2242139422beb69184e65767cac36f33665
electricGebring <electricgebring@gmail.com>
2024-05-17 14:18:19 +0200

    More stuff.

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, jsonify, redirect, url_for, render_template
+from flask import Flask, jsonify, redirect, url_for, render_template, request
 from datetime import datetime
 
+import json
 
-def say_apa():
-	return "Apa"
+
+# Helper function to read and write JSON file
+def read_json():
+    with open('superheroes.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+def write_json(data):
+    with open('superheroes.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/',  methods=['GET'])
 def home():
     return render_template('index.html')
 
@@ -18,6 +27,24 @@ def get_date():
     print("In date function")
     current_date = datetime.now().strftime('%Y-%m-%d')
     return jsonify({'date': current_date})
+
+
+@app.route('/superheroes')
+def superheroes():
+    data = read_json()
+    return render_template('list.html', superheroes=data['superheroes'])
+
+@app.route('/submit_superheroes', methods=['GET', 'POST'])
+def submit_superheroes():
+    if request.method == 'POST':
+        superhero_name = request.form['superhero_name']
+        data = read_json()
+        data['superheroes'].append(superhero_name)
+        write_json(data)
+        # return redirect(url_for('superheroes'))
+        return redirect("/superheroes")
+    else:
+        return render_template('submit.html')
 
 
 if __name__ == '__main__':
